4a9dd56e16f05d7d1fa7bbea295bdc4f482bb6ef
BMP29 <brenomp730@gmail.com>
2023-11-21 09:59:48 -0300

    my github added

--- src/functions/header.js
+++ src/functions/header.js
@@ -53,6 +53,7 @@ const loadHead = () => {
     linkSocial1.setAttribute('href', '#');
     linkSocial2.setAttribute('href', '#');
     linkSocial3.setAttribute('href', '#');
+    //my gitHub
     linkSocial4.setAttribute('href', 'https://github.com/BMP29');
     linkSocial4.setAttribute('target', '_blank');
 
